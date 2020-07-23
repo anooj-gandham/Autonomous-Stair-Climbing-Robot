688349f4723592a1cc5d52f1bc17cce421bd33b2
anooj-gandham <63911217+anooj-gandham@users.noreply.github.com>
2020-07-23 13:48:26 +0530

    Update edge_detect.py

--- edge_detect.py
+++ edge_detect.py
@@ -3,16 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-file = "IMG_20200721_183045.jpg"
+file = "IMG_20200721_183045.jpg" 
+# Import Image file
 img = cv2.imread(file)
 
 t1 = 50
 t2 = 100
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#Detect the increase in gradient with canny
 edges = cv2.Canny(gray, t1, t2)
 
 lines = cv2.HoughLinesP(edges, 1, np.pi/180, 20, maxLineGap=150, minLineLength=1500)
-
+#Probabilistic Hough Lines Transformation
 theta = []
 for line in lines:
     x1, y1, x2, y2 = line[0]
@@ -24,11 +26,14 @@ for line in lines:
         cv2.circle(img, (x2, y2), 30, (0, 255, 0),20)
         cv2.circle(img, (int(np.mean((x1, x2), None)), int(np.mean((y1, y2), None))), 10, (0, 0, 255), 20)
         theta.append(np.arctan((y2-y1)/(x2-x1))*180/np.pi)
+        #calculate slope of each line
 
 
 meantheta = np.mean(theta, None)
 stdtheta = np.std(theta)
 print(meantheta, stdtheta)
+
+#plot the images
 plt.subplot(121), plt.imshow(img, cmap='gray')
 plt.title('Theta: ' + str(round(meantheta,2)) + " +/- " + str(round(stdtheta,2)))
 # plt.xticks([]), plt.yticks([])
